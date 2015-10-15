ec144604d6c2329b6db8bc80e3fbdc927680fd82
JavaWarrior <koko2405000@gmail.com>
2015-10-16 01:31:02 +0200

    fixed free issue

--- tinyShell/TODO.c
+++ tinyShell/TODO.c
@@ -1,9 +1,2 @@
 /*
-1- pressing ctrl+D terminates.
-2- parsing commands.
-3- creating processes.
-4- checking for existance of a directory.
-5- signal handler to check if process was terminated.
-6- log file.
-10- clear spaces quotations
 */
